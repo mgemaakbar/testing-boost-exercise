8e61a099727fbfe6b7d4df1100232e16f1a51622
Muhammad Gema Akbar <mgemaakbar@gmail.com>
2025-02-02 12:45:46 +0100

    Revert back

--- src/matrixIO.cpp
+++ src/matrixIO.cpp
@@ -12,7 +12,7 @@ namespace matrixIO {
 void saveData(const string &fileName, const MatrixXd &matrix)
 {
   // see https://eigen.tuxfamily.org/dox/structEigen_1_1IOFormat.html
-  const static IOFormat CSVFormat(FullPrecision, DontAlignCols, ", ", "\n")
+  const static IOFormat CSVFormat(FullPrecision, DontAlignCols, ", ", "\n");
 
   ofstream file(fileName);
   if (file.is_open()) {
