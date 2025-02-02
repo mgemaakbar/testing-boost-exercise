8a61fa94ff2ca5a0a377c3e5db52782cb35e4aab
Muhammad Gema Akbar <mgemaakbar@gmail.com>
2025-02-02 14:15:23 +0100

    Use fixture

--- tests/MatrixIOTest.cpp
+++ tests/MatrixIOTest.cpp
@@ -5,14 +5,22 @@
 using namespace Eigen;
 using namespace matrixIO;
 
-BOOST_AUTO_TEST_SUITE(MatrixIOTests);
+struct MatrixIOFixture {
+  MatrixIOFixture()
+  {
+    expectedMatrix = MatrixXd(3, 3);
+    expectedMatrix << 0.680375, 0.59688, -0.329554,
+      -0.211234, 0.823295, 0.536459,
+      0.566198, -0.604897, -0.444451;
+
+  }
+  MatrixXd expectedMatrix;
+};
+
+BOOST_FIXTURE_TEST_SUITE(MatrixIOTests, MatrixIOFixture, *boost::unit_test::tolerance(1e-6))
 
 BOOST_AUTO_TEST_CASE(Hoho)
 {
-  MatrixXd expectedMatrix(3, 3);
-  expectedMatrix << 0.680375, 0.59688, -0.329554,
-      -0.211234, 0.823295, 0.536459,
-      0.566198, -0.604897, -0.444451;
 
   MatrixXd resultMatrix = openData("../data/m3.csv", 3);
 
